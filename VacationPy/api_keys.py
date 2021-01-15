7baf942aa42de82bbf3c14cddc0e90ec7330472f
Brandon Tiley <bctiley@gmail.com>
2021-01-15 16:33:52 +1100

    Edited WeatherPy and Finished VacationPy

--- VacationPy/api_keys.py
+++ VacationPy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = "b3d390d3044b16bddb03bc8b36937926"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "AIzaSyApWOcdSyIAOsCIIVzsc8OwaMtVEp42BP0"
--- WeatherPy/api_keys.py
+++ WeatherPy/api_keys.py
@@ -2,4 +2,4 @@
 weather_api_key = "b3d390d3044b16bddb03bc8b36937926"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "AIzaSyApWOcdSyIAOsCIIVzsc8OwaMtVEp42BP0"
